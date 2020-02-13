8f669a23c75e2e3a7c568959eecc5fc1354663b8
AdityA <aditya@Aditya-PC.local>
2020-02-13 16:14:06 +0530

    day1

---
+++ type_number.js
@@ -0,0 +1,17 @@
+const age = 27.5;
+// eslint-disable-next-line no-unused-vars
+const name = 'aditya';
+const a = 10;
+const b = 20;
+const c = 1;
+const d = '1';
+document.write(`sum of age and a = ${a + age} <br>`);
+
+document.write(`sum of c and d = ${c + d}<br>`);
+
+document.write(`sum of b and a = ${a + b}<br>`);
+document.write(`product of c and d = ${c * d}<br>`);
+document.write(`division of c and d = ${c / d}<br>`);
+document.write(`product of a and b = ${a * b}<br>`);
+document.write(`division of a and b = ${a / b}<br>`);
+document.write(`a to the power b = ${a ** b}`); // ** is used for power
