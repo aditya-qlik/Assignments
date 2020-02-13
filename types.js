fa96ee4a8926f14f723e123352016e9495fe75e8
AdityA <aditya@Aditya-PC.local>
2020-02-13 14:32:38 +0530

    day1

--- types.js
+++ types.js
@@ -1,7 +1,13 @@
 console.log('working');
-const first= "double-quotes";
+const first = "double-quotes";
 const second = 'Single-quotes';
 const third = `backtick`;
-
+const job = `full-stack developer`
 // const sentence='i'm a developer';   Gives ERROR it can be corrected by using ""
-const sentence='i\'m a developer \\\\'; // this is called escaping; to print \ we need to put \\
+const sentence = 'i\'m a developer \\\\'; // this is called escaping; to print \ we need to put \\
+const sentence1 = "She's \"so cool\"";
+const sentence2 = `I'm a ${job}`;
+console.log(sentence);
+console.log(sentence1);
+console.log(sentence2);
+document.write(sentence2);
