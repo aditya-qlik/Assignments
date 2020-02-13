d239717e1bc96ced948689fc041791fe7d91e1bc
AdityA <aditya@Aditya-PC.local>
2020-02-13 16:42:00 +0530

    day1

--- type_number.js
+++ type_number.js
@@ -4,7 +4,7 @@ const name = 'aditya';
 const a = 10;
 const b = 20;
 const c = 1;
-const d = '1';
+const d = '4';
 document.write(`sum of age and a = ${a + age} <br>`);
 
 document.write(`sum of c and d = ${c + d}<br>`);
@@ -14,4 +14,6 @@ document.write(`product of c and d = ${c * d}<br>`);
 document.write(`division of c and d = ${c / d}<br>`);
 document.write(`product of a and b = ${a * b}<br>`);
 document.write(`division of a and b = ${a / b}<br>`);
-document.write(`a to the power b = ${a ** b}`); // ** is used for power
+document.write(`a to the power d = ${a ** d}<br>`); // ** is used for power
+document.write(`sum of 0.1 and 0.2 = ${0.1 + 0.2}<br>`); // this is due to floating point
+document.write(`100000 to the power 1000000000 = ${100000 ** 1000000000}<br>`);
