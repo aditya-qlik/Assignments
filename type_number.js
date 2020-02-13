7ef08d94c471a38c9a3c833e5fc5920786462f68
AdityA <aditya@Aditya-PC.local>
2020-02-13 16:47:32 +0530

    day1

--- type_number.js
+++ type_number.js
@@ -17,4 +17,4 @@ document.write(`division of a and b = ${a / b}<br>`);
 document.write(`a to the power d = ${a ** d}<br>`); /* '**' is used for power */
 document.write(`sum of 0.1 and 0.2 = ${0.1 + 0.2}<br>`); // this is due to floating point
 document.write(`100000 to the power 1000000000 = ${100000 ** 1000000000}<br>`); // infinity and -infinity are number
-document.write(`division of number and string = ${10 / 'dog'}<br>`);
+document.write(`division of number and string = ${10 / 'dog'}<br>`); // NaN means not a number
