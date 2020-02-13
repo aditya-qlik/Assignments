b1926d5b5a7c4bf089bcd51f825b1ea529c3152a
AdityA <aditya@Aditya-PC.local>
2020-02-13 17:33:10 +0530

    day1

--- type_object.js
+++ type_object.js
@@ -3,9 +3,15 @@ const person = {
         lname: 'Kumar',
         age: 27,
 };
+
 document.body.innerHTML = `
 <div>
 <h1><center>My Info.</center></h1>
 <h2>My name is ${person.fname} ${person.lname}</h2>
 <h3>My age is ${person.age}</h3>
 </div>`;
+
+// eslint-disable-next-line no-unused-vars
+let somethingundefined;
+// eslint-disable-next-line no-unused-vars
+const somethingnull = null;
