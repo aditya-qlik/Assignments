4fa79b27510988eb928fc6eca5b00db7f743a6b0
AdityA <aditya@Aditya-PC.local>
2020-02-13 13:18:58 +0530

    day1

---
+++ types.js
@@ -0,0 +1,7 @@
+console.log('working');
+const first= "double-quotes";
+const second = 'Single-quotes';
+const third = `backtick`;
+
+// const sentence='i'm a developer';   Gives ERROR it can be corrected by using ""
+const sentence='i\'m a developer \\\\'; // this is called escaping; to print \ we need to put \\
